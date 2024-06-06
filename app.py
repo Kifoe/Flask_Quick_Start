3ca23a04f4aa216cd1bc24a7f8ec0f1dadb6da57
Arsen <vaisovarsen87@gmail.com>
2024-06-06 09:02:37 +0300

    Created ultra simple site

--- app.py
+++ app.py
@@ -13,21 +13,40 @@ def posts():
     return render_template('posts.html', posts=range(1, 11))
 
 
-@app.route("/bye")
-def bye():
-    return f"<h1>Bye, world </h1>"
-
-
-@app.route("/hello/<username>")
-def hello_user(username):
-    return render_template('hello_user.html',username=username)
-
-
 @app.route('/posts/<int:post_id>')
 def show_post(post_id):
     return render_template('show_posts.html', post_id=post_id)
 
 
 @app.route('/login')
-def base():
-    return f"Login"
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route("/user/<username>")
+def hello_user(username):
+    return render_template('hello_user.html', username=username)
+
+
+@app.route('/users')
+def show_users_profile():
+    users = [
+        'kyznetsov',
+        'arsen',
+        'Sovyak'
+    ]
+    return render_template('user_list.html', users=users)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
