c182d0c66efbe0118e4fe560e8c7acc80c48cfa6
Arsen <vaisovarsen87@gmail.com>
2024-06-20 11:18:12 +0300

    Last Fix in login and register validation

--- forms/registration_form.py
+++ forms/registration_form.py
@@ -2,9 +2,12 @@ from wtforms import Form, BooleanField, StringField, PasswordField, validators
 
 
 class RegistrationForm(Form):
-    first_name = StringField('First_name', [validators.Length(min=4, max=15)])
-    last_name = StringField('Last_name', [validators.Length(min=4, max=15)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
+    username = StringField('Username', [
+        validators.Length(min=4, max=25),
+        validators.DataRequired()
+    ])
     password = PasswordField('New Password', [
         validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
     ])
+    confirm = PasswordField('Repear password')
