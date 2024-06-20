75047775a8216723fba683886d1bd8dd51fc5996
Arsen <vaisovarsen87@gmail.com>
2024-06-20 10:36:04 +0300

    Fsxed login and register validation

--- forms/registration_form.py
+++ forms/registration_form.py
@@ -5,6 +5,4 @@ class RegistrationForm(Form):
     username = StringField('Username', [validators.Length(min=4, max=25)])
     password = PasswordField('New Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords must match')
     ])
-    confirm = PasswordField('Repeat Password')
